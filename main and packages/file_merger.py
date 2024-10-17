6eb0eebe58b732668ccc2c37eff297b7bff81256
FilipHolmbrg <148468773+FilipHolmbrg@users.noreply.github.com>
2024-10-17 13:52:09 +0200

    Add files via upload

--- main and packages/file_merger.py
+++ main and packages/file_merger.py
@@ -5,21 +5,9 @@ def merge_file(df_1: pd.DataFrame, df_2: pd.DataFrame, df_3: pd.DataFrame) -> pd
     price_df = df_1
     weather_df = df_2
     inflation_df = df_3
-    # print(price_df.head())
-    # print()
-    # print(weather_df.head())
-    # print()
-    # print(inflation_df.head())
-    # print()
-    
-    # price_df['date'] = pd.to_datetime(price_df['date'])
-    # weather_df['date'] = pd.to_datetime(weather_df['date'])
-    # inflation_df['date'] = pd.to_datetime(weather_df['date'])
-    
-    # merged_df = pd.merge(price_df, weather_df, inflation_df, on='date', how='inner')
+
     merged_df = pd.merge(price_df, weather_df, on='date', how='inner')
     merged_df = pd.merge(merged_df, inflation_df, on='date', how='inner')
     merged_df = merged_df.sort_values(by='date')
     
-    # merged_df.to_excel(r'merged_data.xlsx', index=False)
     return merged_df
